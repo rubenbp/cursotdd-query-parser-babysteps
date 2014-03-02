41b839e7016ca14317fc4a00a5071d2994bca187
Ruben Bernardez <rubenbp@gmail.com>
2014-03-02 18:35:35 +0100

    refactor: extract variable e inicialización en setUp

--- src/com/iexpertos/queryparser/ParserTests.java
+++ src/com/iexpertos/queryparser/ParserTests.java
@@ -3,16 +3,22 @@ package com.iexpertos.queryparser;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParserTests {
+	
+	Parser parser;
+	
+	@Before
+	public void setUp() {
+		parser = new Parser();
+	}
 
 	@Test
 	public void es_insensible_a_mayusculas_minusculas() {	
-		Parser parser = new Parser();
 		List<String> tokens = parser.parse("TEACHER");
 		
 		assertThat(tokens.get(0), equalTo("teacher"));
@@ -20,7 +26,6 @@ public class ParserTests {
 	
 	@Test
 	public void es_insensible_a_mayusculas_minusculas_siempre() {
-		Parser parser = new Parser();
 		List<String> tokens = parser.parse("DOCTOR");
 		
 		assertThat(tokens.get(0), equalTo("doctor"));
