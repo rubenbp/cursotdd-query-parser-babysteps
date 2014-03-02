a372364f6c610f453f90c613e63f7b0b7160c041
Ruben Bernardez <rubenbp@gmail.com>
2014-03-02 18:52:14 +0100

    refactor: extract method

--- src/com/iexpertos/queryparser/Parser.java
+++ src/com/iexpertos/queryparser/Parser.java
@@ -8,10 +8,17 @@ public class Parser {
 	public List<String> parse(String query) {
 		List<String> result = new ArrayList<String>();
 		String token = query.toLowerCase().trim();
-		if (token.endsWith("s")) {
-			token = token.substring(0, token.length() - 1);
-		}
+		token = singularizeToken(token);
 		result.add(token);
 		return result;
 	}
+
+	private String singularizeToken(String token) {
+		if (token.endsWith("s")) {
+			token = token.substring(0, token.length() - 1);
+		}
+		return token;
+	}
+	
+	
 }
